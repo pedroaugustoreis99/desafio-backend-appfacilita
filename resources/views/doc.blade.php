9574d4db08e596d34c46aa5f33f86fef5f900b96
Pedro Augusto <pedroaugustoreis99@gmail.com>
2024-12-16 16:38:32 -0300

    readme e página principal do projeto

---
+++ resources/views/doc.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Desafio back-end</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    @include('layouts.menu')
+    <div class="container mt-5">
+        <h3>Teste back-end</h3>
+        <p>O objetivo desse projeto é demonstrar um pouco dos conhecimentos que eu adquiri em PHP e Laravel nos últimos anos</p>
+        <p><a href="https://www.youtube.com/@pedroaugustoreis99" target="_blank">clique aqui para acessar o meu canal no youtube</a> ou você pode <a href="https://github.com/pedroaugustoreis99" target="_blank">clicar aqui para acessar o meu github com vários projetos</a> que eu já criei</p>
+        <p>Caso tenha alguma dúvida ficarei feliz em bater um papo sobre o projeto</p>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('doc');
 })->name('index');
 
 Route::resource('usuarios', App\Http\Controllers\UsuarioController::class);
